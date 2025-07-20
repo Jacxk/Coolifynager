bf8aa03ed8732c2346dbe21436da73b228c5d94d
Jack <32645451+Jacxk@users.noreply.github.com>
2025-07-20 12:22:44 -0400

    refactor: using SectionList on environment list component

--- components/EnvironmentVariableList.tsx
+++ components/EnvironmentVariableList.tsx
@@ -2,24 +2,53 @@ import { getApplicationEnvs } from "@/api/application";
 import { ApplicationEnv } from "@/api/types/application.types";
 import LoadingScreen from "@/components/LoadingScreen";
 import { Checkbox } from "@/components/ui/checkbox";
-import { Input } from "@/components/ui/input";
+import { Input, PasswordInput } from "@/components/ui/input";
 import { Text } from "@/components/ui/text";
 import { H2 } from "@/components/ui/typography";
-import { useQuery } from "@tanstack/react-query";
-import React from "react";
-import { View } from "react-native";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import React, { useState } from "react";
+import { RefreshControl, SectionList, View } from "react-native";
 import { Card, CardContent } from "./ui/card";
+
+type SectionData = {
+  title: string;
+  data: ApplicationEnv[];
+  description: string;
+};
 
 export default function EnvironmentVariableList({ uuid }: { uuid: string }) {
   const { data: envs, isPending } = useQuery(getApplicationEnvs(uuid));
+
+  const queryClient = useQueryClient();
+  const [isRefreshing, setIsRefreshing] = useState(false);
+
+  const onRefresh = async () => {
+    setIsRefreshing(true);
+    queryClient
+      .invalidateQueries({ queryKey: ["applications", "envs", uuid] })
+      .finally(() => setIsRefreshing(false));
+  };
 
   if (isPending) return <LoadingScreen />;
 
   const previewEnvs = envs?.filter((env) => env.is_preview) || [];
   const productionEnvs = envs?.filter((env) => !env.is_preview) || [];
 
-  const renderEnv = (env: ApplicationEnv) => (
-    <Card key={env.uuid}>
+  const sections: SectionData[] = [
+    {
+      title: "Production Deployments Environment Variables",
+      data: productionEnvs,
+      description: "Environment (secrets) variables for Production.",
+    },
+    {
+      title: "Preview Deployments Environment Variables",
+      data: previewEnvs,
+      description: "Environment (secrets) variables for Preview Deployments.",
+    },
+  ];
+
+  const renderEnv = ({ item: env }: { item: ApplicationEnv }) => (
+    <Card>
       <CardContent className="p-6 gap-2">
         <Input
           placeholder="Key"
@@ -27,12 +56,10 @@ export default function EnvironmentVariableList({ uuid }: { uuid: string }) {
           value={env.key}
           editable={false}
         />
-        <Input
+        <PasswordInput
           placeholder="Value"
           className="text-xs text-muted-foreground"
           value={env.value}
-          textContentType="password"
-          secureTextEntry
         />
         <View className="flex flex-row gap-4 flex-wrap">
           <View className="flex flex-row gap-2">
@@ -64,34 +91,44 @@ export default function EnvironmentVariableList({ uuid }: { uuid: string }) {
     </Card>
   );
 
-  return (
-    <View className="gap-4">
-      <View>
-        <H2 className="font-semibold border-0 pb-1">
-          Production Deployments Environment Variables
-        </H2>
-        <Text className="text-muted-foreground">
-          Environment (secrets) variables for Production.
-        </Text>
-      </View>
-      {productionEnvs.length === 0 ? (
-        <Text className="mb-4">No production environment variables found.</Text>
-      ) : (
-        <View className="gap-2">{productionEnvs.map(renderEnv)}</View>
-      )}
-      <View>
-        <H2 className="font-semibold border-0 pb-1">
-          Preview Deployments Environment Variables
-        </H2>
-        <Text className="text-muted-foreground">
-          Environment (secrets) variables for Preview Deployments.
-        </Text>
-      </View>
-      {previewEnvs.length === 0 ? (
-        <Text>No preview environment variables found.</Text>
-      ) : (
-        <View className="gap-2">{previewEnvs.map(renderEnv)}</View>
-      )}
+  const renderSectionHeader = ({ section }: { section: SectionData }) => (
+    <View className="mb-4 bg-background py-4">
+      <H2 className="font-semibold border-0 pb-1">{section.title}</H2>
+      <Text className="text-muted-foreground">{section.description}</Text>
     </View>
+  );
+
+  const renderEmptySection = ({ section }: { section: SectionData }) => (
+    <Text className="mb-4">
+      No{" "}
+      {section.title.toLowerCase().includes("production")
+        ? "production"
+        : "preview"}{" "}
+      environment variables found.
+    </Text>
+  );
+
+  return (
+    <SectionList
+      automaticallyAdjustKeyboardInsets
+      keyboardShouldPersistTaps="handled"
+      keyboardDismissMode="interactive"
+      sections={sections}
+      renderItem={renderEnv}
+      renderSectionHeader={renderSectionHeader}
+      keyExtractor={(item) => item.uuid}
+      contentContainerClassName="p-4 gap-2"
+      ListEmptyComponent={
+        <Text className="text-center text-muted-foreground">
+          No environment variables found.
+        </Text>
+      }
+      renderSectionFooter={({ section }) =>
+        section.data.length === 0 ? renderEmptySection({ section }) : null
+      }
+      refreshControl={
+        <RefreshControl refreshing={isRefreshing} onRefresh={onRefresh} />
+      }
+    />
   );
 }
