d06dfa021de6a1682b65bb4a760a3510107899ed
Yanluis F <32645451+Jacxk@users.noreply.github.com>
2025-07-25 09:42:24 -0400

    feat: update CreatePrivateAppApplication to indicate unimplemented state

--- app/main/resources/create/application/git/private-app.tsx
+++ app/main/resources/create/application/git/private-app.tsx
@@ -4,8 +4,11 @@ import { View } from "react-native";
 // TODO: Implement github app selection
 export default function CreatePrivateAppApplication() {
   return (
-    <View>
-      <Text>Private App Application</Text>
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-muted-foreground">Not implemented</Text>
+      <Text className="text-muted-foreground">
+        You can still create it on the website.
+      </Text>
     </View>
   );
 }
