f87d64969b8f7882517fd1e8fb1bec6b9b3ecfee
Jack <32645451+Jacxk@users.noreply.github.com>
2025-07-26 08:40:05 -0400

    feat: implement swipeable project card with delete functionality and undo option

--- app/main/projects/index.tsx
+++ app/main/projects/index.tsx
@@ -1,20 +1,87 @@
-import { getProjects } from "@/api/projects";
-import { queryClient } from "@/app/_layout";
+import { deleteProject, getProjects } from "@/api/projects";
+import { ProjectBase } from "@/api/types/project.types";
 import { ResourceCard } from "@/components/cards/ResourceCard";
 import { SafeView } from "@/components/SafeView";
 import { ResourcesSkeleton } from "@/components/skeletons/ProjectsSkeleton";
-import { SwipeGesture } from "@/components/ui/swipe-card";
+import { SwipeableCard } from "@/components/ui/swipe-card";
 import { Text } from "@/components/ui/text";
 import { useRefreshOnFocus } from "@/hooks/useRefreshOnFocus";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { Trash } from "lucide-react-native";
-import { useState } from "react";
-import { FlatList, View } from "react-native";
+import { useRef, useState } from "react";
+import { View } from "react-native";
+import Animated, { LinearTransition } from "react-native-reanimated";
+import { toast } from "sonner-native";
+
+const ProjectCard = (item: ProjectBase) => {
+  const queryClient = useQueryClient();
+  const { mutate } = useMutation(deleteProject(item.uuid));
+
+  const isUndo = useRef(false);
+  const position = useRef(0);
+
+  const handleDismiss = () => {
+    if (!isUndo.current) {
+      mutate();
+      isUndo.current = false;
+    }
+  };
+
+  return (
+    <SwipeableCard
+      rightContentClassName="rounded-r-lg bg-red-400 dark:bg-red-500 -ml-4"
+      rightContent={<Trash color="black" />}
+      dismissOnSwipeLeft
+      onDismiss={() => {
+        queryClient.setQueryData(["projects"], (data: ProjectBase[]) => {
+          const index = data.findIndex((d) => d.uuid === item.uuid);
+          const newData = [...data];
+          newData.splice(index, 1);
+          position.current = index;
+          return newData;
+        });
+
+        queryClient.removeQueries({
+          queryKey: ["projects", item.uuid],
+          exact: true,
+        });
+
+        toast.success("Project deleted", {
+          id: item.uuid,
+          action: {
+            label: "Undo",
+            onClick: () => {
+              isUndo.current = true;
+              queryClient.setQueryData(["projects"], (data: ProjectBase[]) => {
+                const newData = [...data];
+                newData.splice(position.current, 0, item);
+                return newData;
+              });
+              queryClient.setQueryData(["projects", item.uuid], item);
+              toast.dismiss(item.uuid);
+            },
+          },
+          onAutoClose: handleDismiss,
+        });
+      }}
+    >
+      <ResourceCard
+        uuid={item.uuid}
+        title={item.name}
+        description={item.description}
+        type="project"
+        href={{
+          pathname: "/main/projects/[uuid]",
+          params: { uuid: item.uuid, name: item.name },
+        }}
+      />
+    </SwipeableCard>
+  );
+};
 
 export default function ProjectsIndex() {
   const { data, isPending, refetch } = useQuery(getProjects());
   const [isRefreshing, setIsRefreshing] = useState(false);
-
   useRefreshOnFocus(refetch);
 
   if (isPending) {
@@ -31,34 +98,12 @@ export default function ProjectsIndex() {
 
   return (
     <SafeView className="p-0">
-      <FlatList
+      <Animated.FlatList
+        itemLayoutAnimation={LinearTransition}
         contentContainerClassName="p-4"
         data={data}
         keyExtractor={(item) => item.uuid}
-        renderItem={({ item }) => (
-          <SwipeGesture
-            rightContentClassName="rounded-r-lg bg-red-400 dark:bg-red-500"
-            rightContent={<Trash />}
-            onSwipeRight={() =>
-              // TESTING
-              queryClient.setQueryData(
-                ["projects"],
-                data.filter((d) => d.uuid !== item.uuid)
-              )
-            }
-          >
-            <ResourceCard
-              uuid={item.uuid}
-              title={item.name}
-              description={item.description}
-              type="project"
-              href={{
-                pathname: "/main/projects/[uuid]",
-                params: { uuid: item.uuid, name: item.name },
-              }}
-            />
-          </SwipeGesture>
-        )}
+        renderItem={({ item }) => <ProjectCard {...item} />}
         ItemSeparatorComponent={() => <View style={{ height: 12 }} />}
         refreshing={isRefreshing}
         onRefresh={() => {
