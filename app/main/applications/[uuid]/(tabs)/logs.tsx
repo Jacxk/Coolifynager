d126c5ff8636361bccd63bb4df57a6170e7b76f6
Yanluis F <32645451+Jacxk@users.noreply.github.com>
2025-07-28 14:44:07 -0400

    fix: API function calls not using corresponding hooks in logs and deployments

--- app/main/applications/[uuid]/(tabs)/logs.tsx
+++ app/main/applications/[uuid]/(tabs)/logs.tsx
@@ -5,7 +5,7 @@ import { SafeView } from "@/components/SafeView";
 export default function ApplicationLogs() {
   return (
     <SafeView topInset bottomInset={false}>
-      <ResourceLogs logsFetcher={useApplicationLogs} />
+      <ResourceLogs useLogsFetcher={useApplicationLogs} />
     </SafeView>
   );
 }
