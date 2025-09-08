8e224f1bccc4a40941cba6e3560f5a3c281030b7
Yanluis Fermin <32645451+Jacxk@users.noreply.github.com>
2025-09-08 13:23:41 -0400

    feat: integrate expo-application for app versioning and build number

--- constants/AppDetails.ts
+++ constants/AppDetails.ts
@@ -1,12 +1,10 @@
+import * as Application from "expo-application";
 import Constants from "expo-constants";
 
 export const APP_NAME = Constants.expoConfig?.name;
 export const APP_DESCRIPTION = Constants.expoConfig?.description;
-export const APP_VERSION = Constants.expoConfig?.version;
-export const APP_BUILD_VERSION =
-  Constants.expoConfig?.ios?.buildNumber ||
-  Constants.expoConfig?.android?.versionCode ||
-  "1";
+export const APP_VERSION = Application.nativeApplicationVersion;
+export const APP_BUILD_VERSION = Application.nativeBuildVersion;
 export const APP_ICON = require("@/assets/images/icon.png");
 
 export const FAVORITES_STORAGE_KEY = "FAVORITES";
